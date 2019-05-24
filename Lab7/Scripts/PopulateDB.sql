d747aa08c55a7ba35a163ad641e2dd3cf7c3d712
Ancient <yura.ruban2011@gmail.com>
2019-05-24 20:25:18 +0300

    Add populate db script

---
+++ Lab7/Scripts/PopulateDB.sql
@@ -0,0 +1,21 @@
+﻿create table Product (
+	Id int identity(1, 1) primary key,
+	[Name] nvarchar(max) not null,
+	Price float not null
+	)
+
+create table Customer (
+	Id int identity(1, 1) primary key,
+	[Name] nvarchar(max) not null,
+	Age int not null check (Age > 0)
+)
+
+create table [Order] (
+	ProductId int not null,
+	CustomerId int not null,
+	PRIMARY KEY(ProductId, CustomerId),
+	FOREIGN key (ProductId) REFERENCES Product(Id),
+	FOREIGN key (CustomerId) REFERENCES Customer(Id),
+	[Count] int check ([Count] > 0)
+)
+
