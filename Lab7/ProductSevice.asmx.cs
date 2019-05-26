131ff0aa37d5389865c81ae61b1d97ff279841c2
Ancient <yura.ruban2011@gmail.com>
2019-05-26 11:32:45 +0300

    Add CRUD operations for db classes

--- Lab7/ProductSevice.asmx.cs
+++ Lab7/ProductSevice.asmx.cs
@@ -25,9 +25,85 @@ namespace Lab7
         }
 
         [WebMethod]
+        public ProductModel Product(int id)
+        {
+            var prod = productsEntities.Products.Find(id);
+            if (prod == null)
+                return null;
+
+            return new ProductModel(prod);
+        }
+        [WebMethod]
+        public void DeleteProduct(int id)
+        {
+            productsEntities.Products.Remove(productsEntities.Products.Find(id));
+            productsEntities.SaveChanges();
+        }
+
+        [WebMethod]
+        public void UpdateProduct(int id, ProductModel model)
+        {
+            var prod = productsEntities.Products.Find(id);
+            if (prod == null)
+                return;
+
+            prod.Name = model.Name;
+            prod.Price = model.Price;
+            productsEntities.SaveChanges();
+        }
+
+        [WebMethod]
+        public void AddProduct(ProductModel model)
+        {
+            Product product = new Product();
+            product.Name = model.Name;
+            product.Price = model.Price;
+            productsEntities.Products.Add(product);
+            productsEntities.SaveChanges();
+        }
+
+        [WebMethod]
         public List<CustomerModel> Customers()
         {
             return productsEntities.Customers.Select(x => new CustomerModel(x)).ToList();
+        }
+
+        [WebMethod]
+        public CustomerModel Customer(int id)
+        {
+            var cust = productsEntities.Customers.Find(id);
+            if (cust == null)
+                return null;
+
+            return new CustomerModel(cust);
+        }
+        [WebMethod]
+        public void DeleteCustomer(int id)
+        {
+            productsEntities.Customers.Remove(productsEntities.Customers.Find(id));
+            productsEntities.SaveChanges();
+        }
+
+        [WebMethod]
+        public void UpdateCustomer(int id, CustomerModel model)
+        {
+            var cust = productsEntities.Customers.Find(id);
+            if (cust == null)
+                return;
+
+            cust.Name = model.Name;
+            cust.Age = model.Age;
+            productsEntities.SaveChanges();
+        }
+
+        [WebMethod]
+        public void AddCustomer(CustomerModel model)
+        {
+            Customer customer = new Customer();
+            customer.Name = model.Name;
+            customer.Age = model.Age;
+            productsEntities.Customers.Add(customer);
+            productsEntities.SaveChanges();
         }
 
         [WebMethod]
@@ -36,7 +112,45 @@ namespace Lab7
             return productsEntities.Orders.Select(x => new OrderModel(x)).ToList();
         }
 
+        [WebMethod]
+        public OrderModel Order(int id)
+        {
+            var ord = productsEntities.Orders.Find(id);
+            if (ord == null)
+                return null;
 
+            return new OrderModel(ord);
+        }
+        [WebMethod]
+        public void DeleteOrder(int id)
+        {
+            productsEntities.Orders.Remove(productsEntities.Orders.Find(id));
+            productsEntities.SaveChanges();
+        }
+
+        [WebMethod]
+        public void UpdateOrder(int id, OrderModel model)
+        {
+            var ord = productsEntities.Orders.Find(id);
+            if (ord == null)
+                return;
+
+            ord.Count = model.Count;
+            ord.CustomerId = model.CustomerId;
+            ord.ProductId = model.ProductId;
+            productsEntities.SaveChanges();
+        }
+
+        [WebMethod]
+        public void AddOrder(OrderModel model)
+        {
+            Order ord = new Order();
+            ord.Count = model.Count;
+            ord.CustomerId = model.CustomerId;
+            ord.ProductId = model.ProductId;
+            productsEntities.Orders.Add(ord);
+            productsEntities.SaveChanges();
+        }
 
     }
 }
